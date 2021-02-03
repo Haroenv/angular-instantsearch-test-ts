ad5d59ada7ae8b67bb213e545d7f36dc241597e7
Haroen Viaene <hello@haroen.me>
2021-02-03 18:16:22 +0100

    set up angular-instantsearch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import algoliasearch from 'algoliasearch/lite';
+
+const searchClient = algoliasearch(
+  'B1G2GM9NG0',
+  'aadef574be1f9252bb48d4ea09b5cfe5'
+);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'test-angular-app';
+  config = {
+    indexName: 'demo_ecommerce',
+    searchClient: searchClient as any,
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgAisModule } from 'angular-instantsearch';
 
 import { AppComponent } from './app.component';
 
@@ -8,6 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    NgAisModule.forRoot(),
     BrowserModule
   ],
   providers: [],
